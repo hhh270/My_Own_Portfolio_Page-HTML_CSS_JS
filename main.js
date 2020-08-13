7b78b229d9ebaff8dbca87d5a840e43c0f88cda2
Eric Kim <hhh270@naver.com>
2020-08-13 23:49:29 +0900

    Add_style_hide/hover to navbar_JS,CSS
    Add_style_hide/hover to navbar_JS,CSS

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 "use strict";
+
+// Make navbar hide/hover
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  // arrow function 활용 스크롤이 실행될 때, 등록한 함수를 호출
+  //   console.log(window.scrollY);
+  //   console.log(`navbarHeight: ${navbarHeight}`);
+  // result : 67.38
+
+  if (window.scrollY + 60 > navbarHeight) {
+    navbar.classList.add(`navbar__dark`);
+  } else {
+    navbar.classList.remove(`navbar__dark`);
+  }
+  //   후 navbar--dark에 대한 스타일링을 css에서 진행
+});
